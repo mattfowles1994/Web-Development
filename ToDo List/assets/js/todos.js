4293bf9373c151c81514ec27e827b6cb9a4bba0b
Matthew Fowles <matafowles@googlemail.com>
2018-05-02 19:56:38 +0100

    Add files via upload

---
+++ ToDo List/assets/js/todos.js
@@ -0,0 +1,26 @@
+//Check off specific todos by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed")
+})
+
+//click on x to delete todo
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+    	$(this).remove();
+    });
+    event.stopPropagation();
+})
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+        //grab text input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create new li for text
+		$("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>")
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text'").fadeToggle();
+})
